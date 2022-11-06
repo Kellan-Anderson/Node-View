58493c3d8518b1fae2a18df5b02433200cda7a5b
Kellan Anderson <kdanderson5@catamount.wcu.edu>
2022-11-05 20:49:35 -0400

    Working on React-Neo4j integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Graph from './components/Graph';
+import Loader from './components/Loader/Loader'
+import { useReadCypher } from 'use-neo4j';
 import './App.css';
 
 function App() {
-    const d = require("./data/model.json");
+  //const d = require("./data/model.json");
 
-    const [timestep, setTimestep] = useState(1);
-    const [data, setData] = useState(d.timestep1);
+  const [timestep, setTimestep] = useState(1);
+  const [ query, setQuery ] = useState('ts1');
+  //const [data, setData] = useState(d.timestep1);
 
-    const handleChange = (value) => {
-      setTimestep(value);
-      setData(d['timestep' + value]);
-    }
+  const { loading, records, run, error } = useReadCypher(
+    `match (n:${query}), (a:${query})-[]->(b:${query}) with collect(distinct {id: n.id, group: n.group}) as nodes, collect(distinct {source: a.id, target: b.id}) as links return {nodes: nodes, links: links}`,
+  );
 
+  let result = (<div>Loading...</div>);
+
+  let data = {};
+
+  useEffect(() => {
+    run({ query });
+  }, [query]);
+
+  if(error) {
+    console.log("Error:");
+    console.log(error.message);
+    result=(<><div>Error: {error.message}</div><div>query: {query}</div></>)
+  } else if(!loading) {
+    //let data = records[0];//'{nodes: nodes, links: links}');
+    console.log("data from app component:");
+    data = records[0].get('{nodes: nodes, links: links}');
+    console.log( data);
+    //console.log(data);
+    result = (
+      <div>
+        <div>Data loaded successfully</div>
+        
+      </div>
+    )
+  }
+
+  const handleChange = (value) => {
+    setTimestep(value);
+    //setData(d['timestep' + value]);
+    setQuery('ts' + value);
+    console.log("New value: " + value);
+
+  }
+
+//<Graph data={data} />
 
   return (
     <>
+      {result}
       <Graph data={data} />
       <input
         type="range"
         min="1"
-        max="49"
+        max="3"
         onChange={(e) => handleChange(e.target.value)}
         value={timestep} />
       <p>Timestep: {timestep}</p>
@@ -29,3 +67,13 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+  // query:
+  // match p = (a)-[]->(b) with collect({source: a.letter, target: b.letter}) as link_list return {Links: link_list}
+  // match p = (a)-[]->(b) with collect(DISTINCT {id: a.letter, group: a.group}) as nodes, collect({source: a.letter, target: b.letter}) as link_list return {Nodes: nodes, Links: link_list}
+  // match (n), (a)-[]->(b) with collect(distinct {id: n.letter, group: n.group}) as list, collect(distinct {source: a.letter, target: b.letter}) as l return {nodes: list, links: l}
+  // match (n:transaction2), (a:transaction2)-[]->(b:transaction2) with collect(distinct {id: n.letter, group: n.group}) as list, collect(distinct {source: a.letter, target: b.letter}) as l return {nodes: list, links: l}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Neo4jProvider, createDriver } from 'use-neo4j';
+
+const driver = createDriver('neo4j', 'localhost', 7687, 'neo4j', 'password');
 
 const root = document.getElementById('root');
 ReactDOM.render(
-  <App/>, root
+  <React.StrictMode>
+    <Neo4jProvider driver={driver}>
+      <App/>
+    </Neo4jProvider>
+  </React.StrictMode>,
+   root
 );
 
 // If you want to start measuring performance in your app, pass a function
