7983c8ed9580ee5ce21dae805159d2b9df3e47f1
Kellan Anderson <kdanderson5@catamount.wcu.edu>
2022-11-07 09:03:39 -0500

    Graph render working, code needs cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,59 +2,55 @@ import { useEffect, useState } from 'react';
 import Graph from './components/Graph';
 import Loader from './components/Loader/Loader'
 import { useReadCypher } from 'use-neo4j';
+import GetData from './hooks/useData';
 import './App.css';
 
+let FTR = false;
+
 function App() {
-  //const d = require("./data/model.json");
-
   const [timestep, setTimestep] = useState(1);
-  const [ query, setQuery ] = useState('ts1');
-  //const [data, setData] = useState(d.timestep1);
+  const [dataRetrieved, setDataRetrieved] = useState(false);
+  
+  const getQuery = (v) => {
+    const value = 'ts' + v;
+    return `match (n:${value}), (a:${value})-[]->(b:${value}) with collect(distinct {id: n.id, group: n.group}) as nodes, collect(distinct {source: a.id, target: b.id}) as links return {nodes: nodes, links: links}`
+  };
 
-  const { loading, records, run, error } = useReadCypher(
-    `match (n:${query}), (a:${query})-[]->(b:${query}) with collect(distinct {id: n.id, group: n.group}) as nodes, collect(distinct {source: a.id, target: b.id}) as links return {nodes: nodes, links: links}`,
-  );
+  //const data = {nodes: [], links: []};
 
-  let result = (<div>Loading...</div>);
-
-  let data = {};
+  let query = getQuery(timestep);
+  console.log(query); 
+  const {loading, records, run, first} = useReadCypher(query);
 
   useEffect(() => {
-    run({ query });
-  }, [query]);
+    query = getQuery(timestep);
+    run({query});
+  }, [timestep]);
 
-  if(error) {
-    console.log("Error:");
-    console.log(error.message);
-    result=(<><div>Error: {error.message}</div><div>query: {query}</div></>)
-  } else if(!loading) {
-    //let data = records[0];//'{nodes: nodes, links: links}');
-    console.log("data from app component:");
-    data = records[0].get('{nodes: nodes, links: links}');
-    console.log( data);
-    //console.log(data);
-    result = (
-      <div>
-        <div>Data loaded successfully</div>
-        
-      </div>
-    )
+  //console.log("first:", first);
+  //console.log("records:", records);
+
+  let result = (<div>Data not loaded</div>);
+  
+  let data = {};
+  if(records === undefined) {
+    console.log("Records is undefined");
   }
-
+  else {
+    data = records[0].get("{nodes: nodes, links: links}");
+    console.log("data has been assigned:", data);
+    result = (<Graph data={data}/>);
+  }
+  
   const handleChange = (value) => {
     setTimestep(value);
-    //setData(d['timestep' + value]);
-    setQuery('ts' + value);
     console.log("New value: " + value);
-
+    //console.log(data);
   }
-
-//<Graph data={data} />
 
   return (
     <>
-      {result}
-      <Graph data={data} />
+    {result}
       <input
         type="range"
         min="1"
@@ -67,13 +63,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-  // query:
-  // match p = (a)-[]->(b) with collect({source: a.letter, target: b.letter}) as link_list return {Links: link_list}
-  // match p = (a)-[]->(b) with collect(DISTINCT {id: a.letter, group: a.group}) as nodes, collect({source: a.letter, target: b.letter}) as link_list return {Nodes: nodes, Links: link_list}
-  // match (n), (a)-[]->(b) with collect(distinct {id: n.letter, group: n.group}) as list, collect(distinct {source: a.letter, target: b.letter}) as l return {nodes: list, links: l}
-  // match (n:transaction2), (a:transaction2)-[]->(b:transaction2) with collect(distinct {id: n.letter, group: n.group}) as list, collect(distinct {source: a.letter, target: b.letter}) as l return {nodes: list, links: l}
